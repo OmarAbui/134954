97a7d1cf6a08d28f77c6ff8f71c4c30d34c11a36
OmarAbui <108081192+OmarAbui@users.noreply.github.com>
2023-11-28 12:50:03 +0300

    Delete AndroidStudioProjects/FirebaseCourse directory

--- AndroidStudioProjects/FirebaseCourse/app/src/main/java/com/example/firebasecourse/WorkoutFeedbacks.kt
+++
@@ -1,9 +0,0 @@
-package com.example.firebasecourse
-
-data class WorkoutFeedbacks (
-    val wid:String="",
-    val ufid:String="",
-    val fdate:String="",
-    val ftime:String="",
-    val wfeedback:String=""
-)
--- AndroidStudioProjects/FirebaseCourse/app/src/main/java/com/example/firebasecourse/other/SortType.kt
+++
@@ -1,7 +0,0 @@
-package com.example.firebasecourse.other
-
-enum class SortType {
-
-    DATE, RUNNING_TIME, DISTANCE, AVG_SPEED, CALORIES_BURNED
-
-}
